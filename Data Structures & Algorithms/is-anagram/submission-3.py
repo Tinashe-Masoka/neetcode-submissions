d3b72bd1955a3614bd68b27fb5e2cf72c6661de9
Tinashe Masoka <144968526+Tinashe-Masoka@users.noreply.github.com>
2026-05-21 17:55:00 -0600

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        if len(s) != len(t) : return False
+
+        s_dict = {}
+
+        for i in s :
+            s_dict[i] = s_dict.get(i, 0) + 1
+        
+
+        for i in t :
+
+            if i not in s_dict : return False
+            
+            s_dict[i] -= 1
+
+            if s_dict[i] < 0 : return False
+
+
+        return True
+        
+
+
+
+        
