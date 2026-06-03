80799466baf2a9d398378dd83286c00b39213f15
Tinashe Masoka <144968526+Tinashe-Masoka@users.noreply.github.com>
2026-06-03 15:47:59 -0600

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        clean_s = "".join( char.lower() for char in s if char.isalpha() or char.isdigit() )
+        forward = 0
+        backward = len(clean_s) - 1
+
+        while forward < (len(clean_s)//2) :
+
+            if clean_s[forward] != clean_s[backward] :
+                return False
+            forward += 1
+            backward -= 1
+
+        return True
